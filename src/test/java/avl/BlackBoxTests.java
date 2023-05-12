794f062cc264207d0259e6bad44d2027c085628c
LAPTOP-UOADHCKT\Pedro <pedroankersmituni@gmail.com>
2023-05-12 11:08:03 +0200

    Hechos dos tests

--- src/test/java/avl/BlackBoxTests.java
+++ src/test/java/avl/BlackBoxTests.java
@@ -12,15 +12,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BlackBoxTests {
     AvlTree<Integer> tree;
     AvlNode<Integer> node;
-    Comparator<Integer> c;
+    Comparator<Integer> c = Integer::compare;
+
+    @Test
+    @DisplayName("Test del constructor válido")
+    public void whenConstructingATreeWithAValidComparatorItGetsCreated(){
+
+        tree = new AvlTree<>(c);
+
+        assertEquals(null, tree.top);
+    }
+    @Test
+    @DisplayName("Test del constructor inválido")
+    public void whenConstructingATreeWithANotValidComparatorItThrowsException(){
+        c = null;
+        tree = new AvlTree<>(c);
+
+        assertEquals(node, tree.searchNode(node));
+    }
     @Test
     @DisplayName("Test de se inserta un nodo valido")
-    public void t(){
+    public void whenInsertingAValidNodeItGetsAddedInTheTree(){
         tree = new AvlTree<>(c);
         node = new AvlNode<>(1);
+
         tree.insertAvlNode(node);
 
         assertEquals(node, tree.searchNode(node));
-
     }
 }
